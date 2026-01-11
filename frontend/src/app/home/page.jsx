48365f27fd3f5445ca9760678d2261d2fa483afc
Sai Teja Chary Thummalapally <saitejachary1411@gmail.com>
2026-01-11 19:44:28 +0530

    add : integrate CommunityContext to filter posts by joined communities

--- frontend/src/app/home/page.jsx
+++ frontend/src/app/home/page.jsx
@@ -7,11 +7,21 @@ import "../../App.css"
 import { useContext } from "react"
 import { PostContext } from "../../context/PostContext"
 import { PostCardSkeleton } from "../../components/feed/PotCardSkeleton"
+import { CommunityContext } from "../../context/CommunityContext"
 
 export const Home = () => {
     const { globalPosts, loading, handleCommentAdded } = useContext(PostContext)
+    const { communities } = useContext(CommunityContext)
 
     if (loading) return <div>Loading.....</div>
+
+    const joinedCommunityIds = new Set(
+        communities.filter(c => c.joined).map(c => c.id)
+    );
+
+    const joinedPosts = globalPosts.filter(
+        post => joinedCommunityIds.has(post.community.id)
+    );
 
     return (
         <PageContainer>
@@ -19,17 +29,20 @@ export const Home = () => {
             <PageHeader title="Global Feed" description="Join challenge-based communities and stay accountable"
             ><NewPostButton /></PageHeader>
             <div className="main">
-                {loading
-                    ? Array.from({ length: 3 }).map((_, i) => (
-                        <PostCardSkeleton key={i} />
-                    ))
-                    : globalPosts.map(post => (
-                        <PostCard
-                            key={post.postId}
-                            post={post}
-                            onCommentAdded={handleCommentAdded}
-                        />
-                    ))
+                {joinedPosts.length === 0
+                    ? <div>No posts yet from your communities</div>
+                    :
+                    loading
+                        ? Array.from({ length: 3 }).map((_, i) => (
+                            <PostCardSkeleton key={i} />
+                        ))
+                        : joinedPosts.map(post => (
+                            <PostCard
+                                key={post.postId}
+                                post={post}
+                                onCommentAdded={handleCommentAdded}
+                            />
+                        ))
                 }
 
             </div>
